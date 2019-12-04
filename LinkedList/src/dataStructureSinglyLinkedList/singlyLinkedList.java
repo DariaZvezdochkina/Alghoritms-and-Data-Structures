f58e34ca5f2a72ff035c89230c97410197e06a8d
DariaZvezdochkina <57398072+DariaZvezdochkina@users.noreply.github.com>
2019-12-05 00:26:45 +0200

    Update singlyLinkedList.java

--- LinkedList/src/dataStructureSinglyLinkedList/singlyLinkedList.java
+++ LinkedList/src/dataStructureSinglyLinkedList/singlyLinkedList.java
@@ -12,30 +12,30 @@ public class singlyLinkedList {
         return (first == null);
     }
 
-    public void insertFirst(int data) { //используется для вставки узла в начало списка
-        Node newNode = new Node();  //новый узел который будет в структуре данных
-        newNode.data = data;  //данные которые будут записаны в эту структуру
-        newNode.next = first; //новый узел должен соединяться с узлом который раньше был первым
-        first = newNode; //мы указываем на то что новый узел становится первым
+    public void insertFirst(int data) { 
+        Node newNode = new Node();  
+        newNode.data = data;  
+        newNode.next = first; 
+        first = newNode; 
     }
 
-    public void insertLast(int data) { //используется для вставки узла в конец списка
-        Node current = first; //создаем текущий узел который будет первым
+    public void insertLast(int data) { 
+        Node current = first; 
         while (current.next != null) {
-            current = current.next; //мы будем проходить через цикл пока current.next не станет null
+            current = current.next; 
         }
         Node newNode = new Node();
         newNode.data = data;
         current.next = newNode;
     }
 
-    public Node deleteFirst() { //используется для удаления из начала списка
-        Node temp = first;  //для этого нам нужна переменная которая будет указывать на какой либо первый узел
-        first = first.next; //должны изменить все на что указывает эта переменная..таким образом мы указываем на следующее поле справа удаляя все данные о предыдущем узле
+    public Node deleteFirst() { 
+        Node temp = first;  
+        first = first.next; 
         return temp;
     }
 
-    public void displayList() { //будет распечатывать содержимое списка
+    public void displayList() { 
         System.out.println("List (first node --> last node) ");
         Node current = first;
         while (current!=null) {
