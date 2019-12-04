93ea82b1b4dd3f86f4f18fcd851250e38dc530a5
DariaZvezdochkina <57398072+DariaZvezdochkina@users.noreply.github.com>
2019-12-05 00:23:54 +0200

    Update Queue.java

--- Queue/src/dataStructureQueue/Queue.java
+++ Queue/src/dataStructureQueue/Queue.java
@@ -1,30 +1,29 @@
 package dataStructureQueue;
 
 public class Queue {
-    private int maxSize; //для управления количеством ячеек (Queue = line)
-    private long[] queueArray; //ячейки для хранения данных
-    private int front; //позиция индекса элемента спереди на лицевой стороне линии - указатель
-    private int rear; //позиция индекса элемента сзади линии
-    private int nItems; //счетчик для хранения элементов
+    private int maxSize; 
+    private long[] queueArray; 
+    private int front; 
+    private int rear;
+    private int nItems; 
 
-    public Queue(int size) {  //чтобы инициализировать структуру данных с каким то размером, количеством элементов
+    public Queue(int size) { 
         this.maxSize = size;
         this.queueArray = new long[size];
-        front = 0; //индексая позиция первой ячейки массива
-        rear = -1; //в массиве нет элемента который можно было бы считать последним
-        nItems = 0; //у нас пока еще нет элементов в массиве
+        front = 0; 
+        rear = -1;
+        nItems = 0; 
     }
 
-    public void insert(long j) { //нужно вставить данные типо лонг тем самым заполняя линию элементами и вставляя их в конец
-        if (rear==maxSize-1) {  //вызывание перезаписи-циркуляр
-            rear =-1; //если последний элемент будет равен последнему элементу в массиве то следующие вставленные элементы будут вставляться в начало и заменять предыдущие первые
-        }
-        rear++; //с каждым вставленным элементом увеличивается конечный элемент который становится последним
-        queueArray[rear] = j; //если это ноль то мы вставленый элемент присваиваем к нулевому индексу в массиве если первый то к первому и тд
-        nItems++; //количество элементов соответственно тоже увеличивается
+    public void insert(long j) { 
+        if (rear==maxSize-1) {  
+            rear =-1; 
+        rear++; 
+        queueArray[rear] = j; 
+        nItems++; 
     }
 
-    public long remove() { //удаляет элемент спереди линии
+    public long remove() { 
         long temp = queueArray[front];
         front++;
         if (front==maxSize) {
@@ -34,7 +33,7 @@ public class Queue {
         return temp;
     }
 
-    public long peekFront(){ //для заглядывания кто будет в начале линии
+    public long peekFront(){ 
         return queueArray[front];
     }
 
@@ -46,10 +45,10 @@ public class Queue {
         return (nItems==maxSize);
     }
 
-    public void view() { //временная вещь которая используется для просмотра содержимого(всей линии из чисел) в структуре данных
+    public void view() { 
         System.out.print("[ ");
-        for (int i = 0; i < queueArray.length; i++ ) { //мы будем перебирать массив
-            System.out.print(queueArray[i] + " ");  //напечатаем конкретную элемент с позицией индекса i
+        for (int i = 0; i < queueArray.length; i++ ) { 
+            System.out.print(queueArray[i] + " ");  
         }
         System.out.print("]");
     }
