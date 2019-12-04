418bb59a7613eeb71e42a2b11002692891fc8277
DariaZvezdochkina <57398072+DariaZvezdochkina@users.noreply.github.com>
2019-12-05 00:25:42 +0200

    Update Node.java

--- LinkedList/src/dataStructureSinglyLinkedList/Node.java
+++ LinkedList/src/dataStructureSinglyLinkedList/Node.java
@@ -4,7 +4,7 @@ public class Node {
     public int data;
     public Node next;
 
-    public void displayNode() { //будет отображать что в верхних двух переменных
+    public void displayNode() { 
         System.out.println("{ " + data + " } ");
     }
 }
